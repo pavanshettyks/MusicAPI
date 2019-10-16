fe8c4de3467b4447f2fbcddda9bfb7bf564096c6
pavanshettyks <pavanksshetty@gmail.com>
2019-10-15 19:00:41 -0700

    done

--- descriptions.py
+++ descriptions.py
@@ -13,7 +13,7 @@ def make_dicts(cursor, row):
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect('TESTDATABASE')
+        db = g._database = sqlite3.connect('MUSICDATABASE')
         db.row_factory = make_dicts
     db.cursor().execute("PRAGMA foreign_keys=ON")
     return db
@@ -37,7 +37,7 @@ def query_db(query, args=(), one=False):
 def init_db():
     with app.app_context():
         db = get_db()
-        with app.open_resource('test.sql', mode='r') as f:
+        with app.open_resource('music_store.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
